3a31bb35aac5275ec0e76c8ba5b40d57e14beaf8
II <no_soap_0@mail.ru>
2025-11-03 17:41:31 +0300

    add 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
 
-createRoot(document.getElementById("root")!).render(<App />);
+if (window.location.search.startsWith("?redirect=")) {
+    const redirectPath = window.location.search.replace("?redirect=", "")
+    window.history.replaceState(null, "", redirectPath)
+}
+
+createRoot(document.getElementById("root")!).render(<App />)
